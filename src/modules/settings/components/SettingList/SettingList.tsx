336aced180672fdc797daf8ad409783be790b2dc
starodubenko <r.starodubenko@tinkoff.ru>
2019-04-29 14:21:21 +0600

    Reworked SettingsList with using arrow component

--- src/modules/settings/components/SettingList/SettingList.tsx
+++ src/modules/settings/components/SettingList/SettingList.tsx
@@ -1,40 +1,15 @@
-import React, {PureComponent} from "react";
-import {connect} from "react-redux";
+import React from "react";
 import {PeriodSelector} from "../PeriodSelector";
-import {RootState} from "../../../store/model";
 
 import s from "./SettingList.module.scss";
 
-interface InputProps {}
-
-interface StateProps {}
-
-interface DispatchProps {}
-
-type Props = StateProps & DispatchProps & InputProps
-
-interface OwnState {}
-
-export class SettingListComponent extends PureComponent<Props, OwnState> {
-
-  static defaultProps = {};
-
-  render() {
+export const SettingList = () => {
     return (
         <div className={s.Root}>
-          <div className={s.Row}>
-            <span>Data refresh period</span>
-            <span><PeriodSelector /></span>
-          </div>
+            <div className={s.Row}>
+                <span>Data refresh period</span>
+                <span><PeriodSelector /></span>
+            </div>
         </div>
-    );
-  }
-}
-
-const mapStateToProps = (state: RootState, ownProps: InputProps): StateProps => {
-  return {};
+    )
 };
-
-const mapDispatchToProps: DispatchProps = {};
-
-export const SettingList = connect<StateProps, DispatchProps, InputProps, RootState>(mapStateToProps, mapDispatchToProps)(SettingListComponent);
