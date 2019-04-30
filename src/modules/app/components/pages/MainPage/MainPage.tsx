350a14038e7fcc39247b8b34a6c082b0c28a92ed
starodubenko <r.starodubenko@tinkoff.ru>
2019-04-30 19:03:27 +0600

    Reworked MainPage with using arrow component and Hooks

--- src/modules/app/components/pages/MainPage/MainPage.tsx
+++ src/modules/app/components/pages/MainPage/MainPage.tsx
@@ -1,20 +1,19 @@
-import React, {Component} from 'react';
+import React, {useCallback, useMemo, useState} from 'react';
 import {connect} from "react-redux";
 import {SettingList} from "../../../../settings";
 import {
-    addToCurrencyPairList, CurrencyPair,
+    addToCurrencyPairList,
+    CurrencyPair,
     CurrencyPairList,
     currencyPairListSelector,
-    CurrencyPairSelector, Rate, rateListSelector
+    CurrencyPairSelector,
+    Rate,
+    rateListSelector
 } from "../../../../currency";
 import {RootState} from "../../../../store";
-
-import s from './MainPage.module.scss';
 import {RateList} from "../../../../currency/components/RateList";
 
-
-interface InputProps {
-}
+import s from './MainPage.module.scss';
 
 interface StateProps {
     currencyPairList: CurrencyPair[];
@@ -25,64 +24,42 @@ interface DispatchProps {
     addToCurrencyPairList: any;
 }
 
-type Props = StateProps & DispatchProps & InputProps
+type Props = StateProps & DispatchProps
 
-interface OwnState {
-    selectedPairIds: CurrencyPair
-}
+export const MainPageComponent = (props: Props) => {
+    const [selectedPairIds, setSelectedPairIds] = useState(null);
+    const onPairChange = useCallback((currencyPair: CurrencyPair) => {
+        setSelectedPairIds(currencyPair);
+    }, []);
+    const addSelectedCurrencyPair = useCallback(() => {
+        props.addToCurrencyPairList(selectedPairIds);
+        setSelectedPairIds(null);
+    }, [selectedPairIds]);
+    const isPairSelected = useMemo((): boolean => !selectedPairIds, [selectedPairIds]);
 
-export class MainPageComponent extends Component<Props, OwnState> {
-
-    static defaultProps = {};
-
-    state = {
-        selectedPairIds: null
-    };
-
-    onPairChange = (currencyPair: CurrencyPair) => {
-        this.setState({
-            selectedPairIds: currencyPair
-        })
-    };
-
-    addSelectedCurrencyPair = () => {
-        this.props.addToCurrencyPairList(this.state.selectedPairIds);
-        this.setState({
-            selectedPairIds: null
-        })
-    };
-
-    isPairSelected = (): boolean => {
-        return !this.state.selectedPairIds;
-    };
-
-    render() {
-        return (
-            <div className={s.Root}>
-                <SettingList/>
-                <div>
-                    <CurrencyPairSelector onPairChange={this.onPairChange}/>
-                    <button disabled={this.isPairSelected()}
-                            onClick={this.addSelectedCurrencyPair}>Add
-                    </button>
-                    <CurrencyPairList items={this.props.currencyPairList}/>
-                    <RateList list={this.props.rateList}/>
-                </div>
+    return (
+        <div className={s.Root}>
+            <SettingList/>
+            <div>
+                <CurrencyPairSelector onPairChange={onPairChange}/>
+                <button disabled={isPairSelected}
+                        onClick={addSelectedCurrencyPair}>Add
+                </button>
+                <CurrencyPairList items={props.currencyPairList}/>
+                <RateList list={props.rateList}/>
             </div>
-        );
-    }
-}
-
-const mapStateToProps = (state: RootState, ownProps: InputProps): StateProps => {
-    return {
-        currencyPairList: currencyPairListSelector(state),
-        rateList: rateListSelector(state)
-    };
+        </div>
+    )
 };
+
+const mapStateToProps = (state: RootState): StateProps => ({
+    currencyPairList: currencyPairListSelector(state),
+    rateList: rateListSelector(state)
+});
 
 const mapDispatchToProps: DispatchProps = {
     addToCurrencyPairList
 };
 
-export const MainPage = connect<StateProps, DispatchProps, InputProps, RootState>(mapStateToProps, mapDispatchToProps)(MainPageComponent);
+export const MainPage = connect<StateProps, DispatchProps, any, RootState>(mapStateToProps, mapDispatchToProps)(MainPageComponent);
 
